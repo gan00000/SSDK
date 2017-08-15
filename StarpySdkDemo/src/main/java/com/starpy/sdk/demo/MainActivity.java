0014b55eea61061e661f87ef5836de381c4a2a27
ganyuanrong <372129081@qq.com>
2017-08-15 14:51:39 +0800

    更新文档

--- StarpySdkDemo/src/main/java/com/starpy/sdk/demo/MainActivity.java
+++ StarpySdkDemo/src/main/java/com/starpy/sdk/demo/MainActivity.java
@@ -15,7 +15,6 @@ import com.starpy.data.login.response.SLoginResponse;
 import com.starpy.sdk.out.ISdkCallBack;
 import com.starpy.sdk.out.IStarpy;
 import com.starpy.sdk.out.StarpyFactory;
-import com.starpy.sdk.plat.PlatMainActivity;
 
 public class MainActivity extends Activity {
 
@@ -173,7 +172,7 @@ public class MainActivity extends Activity {
         findViewById(R.id.open_plat).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, PlatMainActivity.class));
+                iStarpy.openPlatform(MainActivity.this,"roleLevel","10");
 
             }
         });
