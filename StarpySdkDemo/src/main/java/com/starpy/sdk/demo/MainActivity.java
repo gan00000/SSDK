08b51c97f1dfd674230dc60b548a3dc811b6beb1
ganyuanrong <372129081@qq.com>
2018-12-11 16:48:02 +0800

    完善demo

--- StarpySdkDemo/src/main/java/com/starpy/sdk/demo/MainActivity.java
+++ StarpySdkDemo/src/main/java/com/starpy/sdk/demo/MainActivity.java
@@ -12,13 +12,24 @@ import com.starpy.base.bean.SGameLanguage;
 import com.starpy.base.bean.SPayType;
 import com.starpy.data.login.ILoginCallBack;
 import com.starpy.data.login.response.SLoginResponse;
+import com.starpy.data.login.response.User;
+import com.starpy.sdk.out.IPayCallBack;
+import com.starpy.sdk.out.IRequestUserCallBack;
 import com.starpy.sdk.out.ISdkCallBack;
 import com.starpy.sdk.out.IStarpy;
 import com.starpy.sdk.out.StarpyFactory;
+import com.starpy.thirdlib.facebook.FriendProfile;
+import com.starpy.thirdlib.facebook.SFacebookProxy;
+
+import org.json.JSONObject;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button loginButton, othersPayButton,googlePayBtn,csButton,shareButton;
+
+    List<FriendProfile> mFriendProfiles;
 
     private IStarpy iStarpy;
 
@@ -90,7 +101,12 @@ public class MainActivity extends AppCompatActivity {
                 roleLevel 觉得等级
                 customize 自定义透传字段（从服务端回调到cp）
                 */
-                iStarpy.pay(MainActivity.this, SPayType.OTHERS, "" + System.currentTimeMillis(), "payone", "roleLevel", "customize");
+                iStarpy.pay(MainActivity.this, SPayType.OTHERS, new IPayCallBack() {
+                    @Override
+                    public void onFinish() {
+                        PL.d("other pay onFinish");
+                    }
+                },"", "", "", "");
 
 
             }
@@ -108,19 +124,23 @@ public class MainActivity extends AppCompatActivity {
                 roleLevel 觉得等级
                 customize 自定义透传字段（从服务端回调到cp）
                 */
-                iStarpy.pay(MainActivity.this, SPayType.GOOGLE, "" + System.currentTimeMillis(), "tw.mthx.100usd", "roleLevel", "customize");
+                iStarpy.pay(MainActivity.this, SPayType.GOOGLE, new IPayCallBack() {
+                    @Override
+                    public void onFinish() {
+                        PL.d("google pay onFinish");
+                    }
+                },"" + System.currentTimeMillis(), "tw.mthx.100usd", "roleLevel", "customize");
 
             }
         });
 
-        http://localhost:8086/player_entrance?gameCode=brmmd&packageName=web&userId=2&accessToken=123&loginTimestamp=234&serverCode=1&roleName=%E9%A9%AC%E7%BA%A2%E5%86%9B
         csButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 /**
                  * 打开客服接口
                  * level：游戏等级
-                 * vipLevel：vip等级，没有就选""
+                 * vipLevel：vip等级，没有就传""
                  */
                 iStarpy.cs(MainActivity.this,"level","vipLevel");
             }
@@ -147,7 +167,7 @@ public class MainActivity extends AppCompatActivity {
                     }
                 };
 
-                iStarpy.share(MainActivity.this,iSdkCallBack,"", "", shareUrl, "");
+                iStarpy.share(MainActivity.this,iSdkCallBack,shareUrl);
 
             }
         });
@@ -162,6 +182,7 @@ public class MainActivity extends AppCompatActivity {
                  * vipLevel：vip等级，没有就写""
                  */
                 iStarpy.openWebview(MainActivity.this,"roleLevel","10");
+
             }
         });
 
@@ -224,7 +245,161 @@ public class MainActivity extends AppCompatActivity {
                 iStarpy.displayLeaderboard("CgkIq8GizdAREAIQHg");
             }
         });
+
+        findViewById(R.id.get_fb_friends).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /**
+                 * 获取我的fb好友
+                 */
+                iStarpy.getFacebookFriends(MainActivity.this, new SFacebookProxy.RequestFriendsCallBack() {
+                    @Override
+                    public void onError() {
+
+                    }
+
+                    /**
+                     * 回调friendProfiles为好友对象列表
+                     * @param graphObject
+                     * @param friendProfiles
+                     */
+                    @Override
+                    public void onSuccess(JSONObject graphObject, List<FriendProfile> friendProfiles) {
+                        for (FriendProfile fbFriend:friendProfiles) {
+                            String name = fbFriend.getName();//FB好有名字
+                            String iconUrl = fbFriend.getIconUrl();//FB好友头像url
+                            String idToken = fbFriend.getId();//好友的fbIdToken
+                            String userId = fbFriend.getUserId();//好友在starpy平台的账号id,即好友在玩该游戏的starpy平台的账号id，如果fb好友没有玩，此字段为NULL
+                            boolean isStarpyUser = fbFriend.isStarpyUser();//可以判断该好友是否是starpy平台的用户，即是否玩过该游戏
+                        }
+
+
+                    }
+                });
+            }
+        });
+
+        findViewById(R.id.invite_fb_friends).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                /**
+                 * 邀请好友
+                 * mFriendProfiles   需要邀请的好友对象列表(已经进入过该游戏的好友不可以再邀请，需要过滤，不给玩家选择)
+                 * message  发送邀请的内容
+                 * FbInviteFriendsCallBack  邀请回调
+                 */
+                iStarpy.inviteFriends(MainActivity.this, mFriendProfiles, "好玩哦", new SFacebookProxy.FbInviteFriendsCallBack() {
+                    @Override
+                    public void onCancel() {
+
+                    }
+
+                    @Override
+                    public void onError(String message) {
+
+                    }
+
+                    @Override
+                    public void onSuccess(String requestId, List<String> requestRecipients) {
+                        //里面回传的参数不需要处理
+                    }
+                });
+
+            }
+        });
+
+        findViewById(R.id.showad).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                /**
+                 * 获得通过fb分享连接安装进游戏的用户
+                 */
+                iStarpy.showAd(MainActivity.this, new SFacebookProxy.FbAdCallBack() {
+                    @Override
+                    public void onInterstitialDisplayed() {
+
+                    }
+
+                    @Override
+                    public void onInterstitialDismissed() {
+
+                    }
+
+                    @Override
+                    public void onAdClicked() {
+
+                    }
+                });
+
+            }
+        });
+
+        findViewById(R.id.get_invite_user).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                /**
+                 * 获得通过fb分享连接安装进游戏的用户
+                 */
+                iStarpy.getInviteUser(MainActivity.this, new IRequestUserCallBack() {
+                    @Override
+                    public void onFinish(List<User> users) {
+                        if (users != null && !users.isEmpty()){
+                            //continue to do
+                        }
+                    }
+                });
+
+            }
+        });
+        findViewById(R.id.fb_share_ad).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                /**
+                 * 该接口为fb分享到达一定次数后弹出fb广告接口
+                 * ISdkCallBack 为分享回调
+                 * shareLinkUrl为分享的链接
+                 * FbAdCallBack 为广告的回调
+                 */
+                iStarpy.fbShareAndShowAd(MainActivity.this,
+                        new ISdkCallBack() {
+                            @Override
+                            public void success() {
+
+                            }
+
+                            @Override
+                            public void failure() {
+
+                            }
+                        },
+                        "https://developers.facebook.com/docs/facebook-login/review/examples/#example",
+
+                        new SFacebookProxy.FbAdCallBack() {
+                            @Override
+                            public void onInterstitialDisplayed() {
+                                //广告显示
+                            }
+
+                            @Override
+                            public void onInterstitialDismissed() {
+                                //广告消失
+                            }
+
+                            @Override
+                            public void onAdClicked() {
+                                //广告被点击
+                            }
+                        });
+
+            }
+        });
     }
+
+
 
     @Override
     protected void onResume() {
